33736a5110e2e9fde9e2fbd06bb8e95268ef4ee4
TBLStudio <iosdev.tbl@gmail.com>
2015-11-06 14:36:33 +0700

    #3. Simple TableView

---
+++ #3. Simple TableView/SimpleTableView/SimpleTableView/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  SimpleTableView
+//
+//  Created by Thái Bô Lão on 11/6/15.
+//  Copyright © 2015 TBLStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        
+        cell.textLabel?.text = "Cell \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+
+}
+
